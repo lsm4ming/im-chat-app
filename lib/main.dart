ef6633ffad8010507683aae7ca8136b3ba6aad10
lsm <487005831@qq.com>
2024-05-30 23:58:58 +0800

    init

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:chat/screens/welcome/welcome_screen.dart';
+import 'package:chat/theme.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'IM 应用',
+      debugShowCheckedModeBanner: false,
+      theme: lightThemeData(context),
+      darkTheme: darkThemeData(context),
+      themeMode: ThemeMode.light,
+      home: const WelcomeScreen(),
+    );
+  }
+}
---
+++ lib/screens/chats/chats_screen.dart
@@ -0,0 +1,69 @@
+import 'package:chat/constants.dart';
+import 'package:flutter/material.dart';
+
+import 'components/body.dart';
+
+class ChatsScreen extends StatefulWidget {
+  const ChatsScreen({Key? key}) : super(key: key);
+
+  @override
+  _ChatsScreenState createState() => _ChatsScreenState();
+}
+
+class _ChatsScreenState extends State<ChatsScreen> {
+  int _selectedIndex = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: buildAppBar(),
+      body: const Body(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: kPrimaryColor,
+        child: const Icon(
+          Icons.person_add_alt_1,
+          color: Colors.white,
+        ),
+      ),
+      bottomNavigationBar: buildBottomNavigationBar(),
+    );
+  }
+
+  BottomNavigationBar buildBottomNavigationBar() {
+    return BottomNavigationBar(
+      type: BottomNavigationBarType.fixed,
+      currentIndex: _selectedIndex,
+      onTap: (value) {
+        setState(() {
+          _selectedIndex = value;
+        });
+      },
+      items: const [
+        BottomNavigationBarItem(icon: Icon(Icons.messenger), label: "消息"),
+        BottomNavigationBarItem(icon: Icon(Icons.people), label: "好友"),
+        BottomNavigationBarItem(icon: Icon(Icons.call), label: "电话"),
+        BottomNavigationBarItem(
+          icon: CircleAvatar(
+            radius: 14,
+            backgroundImage: AssetImage("assets/images/user_2.png"),
+          ),
+          label: "我的",
+        ),
+      ],
+    );
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+      backgroundColor: kPrimaryColor,
+      automaticallyImplyLeading: false,
+      title: const Text("聊天"),
+      actions: [
+        IconButton(
+          icon: const Icon(Icons.search),
+          onPressed: () {},
+        ),
+      ],
+    );
+  }
+}
